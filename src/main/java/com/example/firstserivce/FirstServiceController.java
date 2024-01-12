f3eba0908e6027ad09d5e8ca14e19d0cbb2845de
sesam-me <lsi6601@gmail.com>
2024-01-13 03:34:33 +0900

    Feat: Load Balancer

--- src/main/java/com/example/firstserivce/FirstServiceController.java
+++ src/main/java/com/example/firstserivce/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.example.firstserivce;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +31,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        // 💡 port 가져오는 방법1. HttpServletRequest
+        log.info("Server port={}", request.getServerPort());
+        // 💡 port 가져오는 방법2. 환경변수에 할당된 정보값을 LocalServerPort로 가져오기
+        return String.format("Hi, there. This is a message from First Service on PORT %s.", env.getProperty("local.server.port"));
     }
 }
